7b9f5889e52f23dfb600b044b25cb992ec2790a9
ullallulla <aleksi.ojansivu@gmail.com>
2024-04-19 14:46:53 +0900

    ex 5.1-5.4

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import ErrorMessage from './components/ErrorMessage'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newURL, setNewURL] = useState('')
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -24,6 +30,30 @@ const App = () => {
       blogService.setToken(user.token)
     }
   }, [])
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newURL
+    }
+
+    try {
+      const newBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(newBlog))
+      setNotification(`a new blog ${newTitle} by ${newAuthor} added`)
+      setNewTitle('')
+      setNewAuthor('')
+      setNewURL('')
+      setTimeout(() => {
+        setNotification(null)
+      }, 3000)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -45,6 +75,17 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleURLChange = (event) => {
+    setNewURL(event.target.value)
   }
 
   const handleLogout = async () => {
@@ -77,11 +118,36 @@ const App = () => {
       <button id='login-button' type="submit">login</button>
     </form>
   )
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+      title: <input
+        value={newTitle}
+        onChange={handleTitleChange}
+      />
+      </div>
+      <div>
+      author: <input
+        value={newAuthor}
+        onChange={handleAuthorChange}
+      />
+      </div>
+      <div>
+      url: <input
+        value={newURL}
+        onChange={handleURLChange}
+      />
+      </div>
+      <button type="submit">save</button>
+    </form>  
+  )
   
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
+        <ErrorMessage message={errorMessage} />
         {loginForm()}
       </div>
     )
@@ -89,8 +155,13 @@ const App = () => {
 
   return (
     <div>
-      <p>{user.name} logged-in</p> <button onClick={handleLogout}>logout</button>
       <h2>blogs</h2>
+      <Notification message={notification} />
+      <p>{user.name} logged-in</p> <button onClick={handleLogout}>logout</button>
+
+      <h2>create new</h2>
+      {blogForm()}
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
