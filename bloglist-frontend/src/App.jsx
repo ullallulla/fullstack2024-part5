2a0aa4709ace395f8d1c43ca0ad302255629c778
ullallulla <aleksi.ojansivu@gmail.com>
2024-04-22 14:23:20 +0900

    ex 5.6

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,25 +1,30 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
 import ErrorMessage from './components/ErrorMessage'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [newTitle, setNewTitle] = useState('')
   const [newAuthor, setNewAuthor] = useState('')
   const [newURL, setNewURL] = useState('')
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [notification, setNotification] = useState(null)
+  const [blogVisible, setBlogVisible] = useState(false)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    const fetchBlogs = async () => {
+      const initialBlogs = await blogService.getAll()
+      setBlogs(initialBlogs)
+    }
+    fetchBlogs()
   }, [])
 
   useEffect(() => {
@@ -31,22 +36,12 @@ const App = () => {
     }
   }, [])
 
-  const addBlog = async (event) => {
-    event.preventDefault()
-
-    const blogObject = {
-      title: newTitle,
-      author: newAuthor,
-      url: newURL
-    }
+  const addBlog = async (blogObject) => {
 
     try {
       const newBlog = await blogService.create(blogObject)
       setBlogs(blogs.concat(newBlog))
-      setNotification(`a new blog ${newTitle} by ${newAuthor} added`)
-      setNewTitle('')
-      setNewAuthor('')
-      setNewURL('')
+      setNotification(`a new blog ${blogObject.title} by ${blogObject.author} added`)
       setTimeout(() => {
         setNotification(null)
       }, 3000)
@@ -119,30 +114,34 @@ const App = () => {
     </form>
   )
 
-  const blogForm = () => (
-    <form onSubmit={addBlog}>
+  const blogForm = () => {
+
+    const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+    const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+    return (
       <div>
-      title: <input
-        value={newTitle}
-        onChange={handleTitleChange}
-      />
+        <div div style={hideWhenVisible}>
+          <button onClick={() => setBlogVisible(true)}>
+            new blog
+          </button>
+        </div>
+        <div div style={showWhenVisible}>
+          <BlogForm
+            createBlog={addBlog}
+            handleAuthorChange={({ target }) => setNewAuthor(target.value)}
+            handleTitleChange={({ target }) => setNewTitle(target.value)}
+            handleURLChange={({ target }) => setNewURL(target.value)}
+            newAuthor={newAuthor}
+            newTitle={newTitle}
+            newURL={newURL}
+          />
+          <button onClick={() => setBlogVisible(false)}>cancel</button>
+        </div>
       </div>
-      <div>
-      author: <input
-        value={newAuthor}
-        onChange={handleAuthorChange}
-      />
-      </div>
-      <div>
-      url: <input
-        value={newURL}
-        onChange={handleURLChange}
-      />
-      </div>
-      <button type="submit">save</button>
-    </form>  
-  )
-  
+    )
+  }
+
   if (user === null) {
     return (
       <div>
@@ -158,10 +157,9 @@ const App = () => {
       <h2>blogs</h2>
       <Notification message={notification} />
       <p>{user.name} logged-in</p> <button onClick={handleLogout}>logout</button>
-
-      <h2>create new</h2>
-      {blogForm()}
-
+      <Togglable buttonLabel='new blog'>
+        <BlogForm createBlog={addBlog} />
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+
+const BlogForm = ({ createBlog }) => {
+    const [newTitle, setNewTitle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
+    const [newURL, setNewURL] = useState('')
+
+
+    const addBlog = async (event) => {
+        event.preventDefault()
+
+        createBlog({
+            title: newTitle,
+            author: newAuthor,
+            url: newURL
+        })
+        setNewTitle('')
+        setNewAuthor('')
+        setNewURL('')
+    }
+
+
+return (
+    <div>
+        <h2>create new</h2>
+        <form onSubmit={addBlog}>
+            <div>
+                title: <input
+                    value={newTitle}
+                    onChange={event => setNewTitle(event.target.value)}
+                />
+            </div>
+            <div>
+                author: <input
+                    value={newAuthor}
+                    onChange={event => setNewAuthor(event.target.value)}
+                />
+            </div>
+            <div>
+                url: <input
+                    value={newURL}
+                    onChange={event => setNewURL(event.target.value)}
+                />
+            </div>
+            <button type="submit">save</button>
+        </form>
+    </div>
+)
+}
+
+export default BlogForm
+
+
+
