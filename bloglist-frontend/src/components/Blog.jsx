55374ef6e99be73cc1ff484f046d7ea54c8ab2f3
ullallulla <aleksi.ojansivu@gmail.com>
2024-04-22 15:13:46 +0900

    ex 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div div style={blogStyle}>
+      <div div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div div style={showWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>hide</button> <br />
+        {blog.url} <br />
+        {blog.likes}
+        <button>like</button> <br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
