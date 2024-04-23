5e806215be66a6761ec419990d095c960eaa04c5
ullallulla <aleksi.ojansivu@gmail.com>
2024-04-23 12:01:35 +0900

    ex 5.10

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -169,7 +169,7 @@ const App = () => {
       <Togglable buttonLabel='new blog'>
         <BlogForm createBlog={addBlog} />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} updateBlogLikes={updateLikes}/>
       )}
     </div>
